ab4b8a510301a40485051c37edd130fea938fa74
Roland Chernov <mail@roland.black>
2018-01-25 07:21:25 +0000

    Change token

--- app/src/main/java/ru/toborob/app1/MainActivity.java
+++ app/src/main/java/ru/toborob/app1/MainActivity.java
@@ -5,27 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import com.microsoft.azure.mobile.MobileCenter;
-import com.microsoft.azure.mobile.analytics.Analytics;
-import com.microsoft.azure.mobile.crashes.Crashes;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "ru.toborob.app1.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MobileCenter.start(getApplication(), "2b2da6fb-a979-41d6-8805-11af4da13187",
-                Analytics.class, Crashes.class);
+        AppCenter.start(getApplication(), "9d4c4622-a489-48d6-adf6-c8b22cdec83c", Analytics.class, Crashes.class);
         setContentView(R.layout.activity_main);
     }
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        Analytics.trackEvent(message);
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
     }
 
 }
